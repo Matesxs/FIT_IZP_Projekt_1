490dbcdcd048f9e141f6cf1afb2f6ad6afcafe00
Martin Dousa <martindousa186@gmail.com>
2020-10-09 21:05:21 +0200

    ...

--- sheet.c
+++ sheet.c
@@ -27,6 +27,15 @@ const char *AREA_SELECTOR_COMS[] = {"rows", "beginswith", "contains"};
 
 enum Mode {PASS, TABLE_EDIT, DATA_EDIT};
 
+struct line_struct
+{
+  char *line_string;
+  int line_index;
+  int num_of_cols;
+  char delim;
+  int last_line;
+};
+
 int get_table_edit_com_index(char *com)
 {
   for (int i=0; i < NUMBER_OF_TABLE_EDIT_COMS; i++)
@@ -167,6 +176,28 @@ int getDelims(char *input_array[], int array_len, char *delim)
   return -3;
 }
 
+void replace_unused_delims(char *string, char* delims)
+{
+  /*
+  Iterate over string and replace delims that are not on 0 position in delims string with delim on 0 position
+
+  params:
+  :string - string where to replace delims
+  :delims - string with delims
+  */
+
+  for (size_t i=0; i < strlen(string); i++)
+  {
+    for (size_t j=1; j < strlen(delims); j++)
+    {
+      if (string[i] == delims[j])
+      {
+        string[i] = delims[0];
+      }
+    }
+  }
+}
+
 int count_specific_chars(char *string, char ch)
 {
   /*
@@ -231,28 +262,6 @@ int get_position_of_character(char *string, char ch, int index)
   return -1;
 }
 
-void replace_unused_delims(char *string, char* delims)
-{
-  /*
-  Iterate over string and replace delims that are not on 0 position in delims string with delim on 0 position
-
-  params:
-  :string - string where to replace delims
-  :delims - string with delims
-  */
-
-  for (size_t i=0; i < strlen(string); i++)
-  {
-    for (size_t j=1; j < strlen(delims); j++)
-    {
-      if (string[i] == delims[j])
-      {
-        string[i] = delims[0];
-      }
-    }
-  }
-}
-
 int get_sub_string(char *string, char delim, int index, char *substring, int max_length)
 {
   /*
@@ -314,6 +323,16 @@ int get_sub_string(char *string, char delim, int index, char *substring, int max
   return 0;
 }
 
+void check_line_length(struct line_struct line)
+{
+  // Check if length of line isnt larger than max size of line
+  if (strlen(line.line_string) > MAX_LINE_LEN)
+  {
+    fprintf(stderr, "\nLine %d exceded max memory size! Max length of line is %d characters (including delims)\n", line.line_index+1, MAX_LINE_LEN);
+    exit(-5);
+  }
+}
+
 int string_to_double(char *string, double *val)
 {
   /*
@@ -355,7 +374,7 @@ int string_to_int(char *string, int *val)
   return 0;
 }
 
-int argument_to_int(char *input_array[], int array_len, int index)
+int argument_to_unsigned_int(char *input_array[], int array_len, int index)
 {
   int val;
   if (index > (array_len + 1)) return -1;
@@ -363,6 +382,56 @@ int argument_to_int(char *input_array[], int array_len, int index)
   if (val <= 0) return -3;
 
   return val;
+}
+
+void process_line(struct line_struct line, int argc, char *argv[], int operating_mode)
+{
+  for (int i=1; i < argc; i++)
+  {
+    switch (operating_mode)
+    {
+    case TABLE_EDIT:
+      switch (get_table_edit_com_index(argv[i]))
+      {
+      case 5:
+        break;
+      
+      default:
+        break;
+      }
+      break;
+
+    case DATA_EDIT:
+      break;
+
+    default:
+      break;
+    }
+  }
+
+  if (line.line_string[0] != 0)
+    printf("%s\n", line.line_string);
+
+  if (line.last_line)
+  {
+    if (operating_mode == TABLE_EDIT)
+    {
+      for (int i=1; i < argc; i++)
+      {
+        if (get_table_edit_com_index(argv[i]) == 1)
+        {
+          for (int j=0; j < line.num_of_cols; j++)
+          {
+            
+            if (j < (line.num_of_cols - 1))
+              printf("%c", line.delim);
+          }
+
+          printf("\n");
+        }
+      }
+    }
+  }
 }
 
 int main(int argc, char *argv[])
@@ -394,78 +463,40 @@ int main(int argc, char *argv[])
 
   // Create buffer strings for line and cell
   char line[MAX_LINE_LEN + LINE_TEST_OFFSET];
-  char cell[MAX_CELL_LEN];
+  char buffer_line[MAX_LINE_LEN + LINE_TEST_OFFSET];
 
-  int number_of_columns = 0;
-  int line_index = 0;
+  if (fgets(buffer_line, (MAX_LINE_LEN + LINE_TEST_OFFSET), stdin) == NULL)
+  {
+    printf("Input cant be empty");
+    return -4;
+  }
 
-  // int mode = get_operating_mode(argv, argc);
+  int operating_mode = get_operating_mode(argv, argc);
+
+  struct line_struct line_holder;
+  line_holder.delim = delims[0];
+  line_holder.num_of_cols = get_number_of_cells(buffer_line, delims[0]);
 
   // Iterate over lines
-  while (fgets(line, (MAX_LINE_LEN + LINE_TEST_OFFSET), stdin) != NULL)
+  while (!line_holder.last_line)
   {
-    // Get number of cols from first line
-    if (line_index == 0)
-    {
-      number_of_columns = get_number_of_cells(line, delims[0]);
-    }
-
-    // Check if length of line isnt larger than max size of line
-    if (strlen(line) > MAX_LINE_LEN)
-    {
-      fprintf(stderr, "\nLine %d exceded max memory size! Max length of line is %d characters (including delims)\n", line_index+1, MAX_LINE_LEN);
-      return -4;
-    }
+    strcpy(line, buffer_line);
+    line_holder.last_line = fgets(buffer_line, (MAX_LINE_LEN + LINE_TEST_OFFSET), stdin) == NULL;
 
     remove_newline_character(line);
     replace_unused_delims(line, delims);
+    line_holder.line_string = line;
 
-    // Iterate over cells in line
-    for (int i=0; i < number_of_columns; i++)
-    {
-      switch (get_sub_string(line, delims[0], i, cell, MAX_CELL_LEN))
-      {
-      case -1:
-        fprintf(stderr, "\nLine %d cell %d exceded max length of cell! Max length of cell is %d characters (excluding delims)\n", line_index+1, i+1, MAX_CELL_LEN);
-        return -5;
-        break;
+    check_line_length(line_holder);
+    process_line(line_holder, argc, argv, operating_mode);
 
-      // If cell is not found then insert empty one
-      case -2:
-        if (i < (number_of_columns - 1))
-          printf("%c", delims[0]);
-        continue;
-        break;
-      
-      default:
-        break;
-      }
-
-      if (i < (number_of_columns - 1))
-      {
-        printf("%s%c", cell, delims[0]);
-      }
-      else
-      {
-        printf("%s", cell);
-      }
-    }
-
-    line_index++;
-    printf("\n");
-  }
-
-  // Check if input us not empty (It doesnt matter its after processing because nothing will be processed if input is empty)
-  if (line_index == 0)
-  {
-    printf("Input cant be empty");
-    return -5;
+    line_holder.line_index++;
   }
 
   //Debug
   printf("\n\nDebug:\n");
 
-  printf("Num of cols: %d\n", number_of_columns);
+  printf("Num of cols: %d\n", line_holder.num_of_cols);
 
   printf("Args: ");
   for (int i=1; i < argc; i++)
